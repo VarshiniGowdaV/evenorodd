cb7ffbad2334ee1fd27de054a850c70926b78e52
VarshiniGowdaV <varshinigowdav14@gmail.com>
2024-10-10 10:33:28 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    if(num%2 == 0)
+    {
+        printf("%d is even\n",num);
+    }
+    else
+    {
+        printf("%d is odd\n",num);
+    }
+    return 0;
+}
